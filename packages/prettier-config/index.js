11bdf876e2edf05cc244264dfb9033031092ff25
Oleg Pimenov <opimenov@ptsecurity.com>
2021-07-09 17:55:58 +0300

    chore: обновление по инфраструктуре

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,28 +1,33 @@
 module.exports = {
+    // max 120 characters per line
     printWidth: 120,
+    // use spaces instead of indentations
+    useTabs: false,
     tabWidth: 4,
     singleQuote: true,
     trailingComma: "none",
+    // lf for newline
+    endOfLine: "lf",
     overrides: [
         {
             files: '*.html',
             options: {
                 parser: 'angular',
-                htmlWhitespaceSensitivity: 'ignore',
-            },
+                htmlWhitespaceSensitivity: 'ignore'
+            }
         },
         {
             files: '*.scss',
             options: {
                 parser: 'scss',
-                singleQuote: false,
-            },
+                singleQuote: false
+            }
         },
         {
             files: '*.ts',
             options: {
-                parser: 'babel-ts',
-            },
-        },
-    ],
+                parser: 'babel-ts'
+            }
+        }
+    ]
 };
