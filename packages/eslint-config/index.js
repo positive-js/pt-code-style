fffeb192380072effe7bcae3ab47100ebd0ecc95
Alexander Kaminskiy <akaminskiy@ptsecurity.com>
2021-11-02 12:32:38 +0300

    feat(eslint): Добавлена базовая конфигурация для html

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,11 +1,13 @@
 const baseConfig = require('./base');
 const typescript = require('./overrides/typesript');
 const javascript = require('./overrides/javascript');
+const html = require('./overrides/html');
 
 module.exports = {
     ...baseConfig,
     overrides: [
         typescript,
-        javascript
+        javascript,
+        html
     ]
 };
